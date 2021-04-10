1e8493e479d1e2991941d8ee8b7f22d11a8dfe79
Youssef BenAli <youyssef@gmail.com>
2021-04-10 12:47:07 +0100

    implementing pokemon details fetching

--- src/api/pokemon.js
+++ src/api/pokemon.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import axiosInstance from "../api/axios";
 
 export const fetchInfinitePokemons = async ({ pageParam = 1 }) => {
@@ -5,6 +6,12 @@ export const fetchInfinitePokemons = async ({ pageParam = 1 }) => {
     `?limit=20&offset=${(pageParam - 1) * 20}`
   );
   const { results } = data;
+  console.log(pageParam);
 
   return { pokemons: results, pageParam: pageParam + 1 };
+};
+
+export const fetchPokemonItem = async (url) => {
+  const { data } = await axios.get(url);
+  return data;
 };
